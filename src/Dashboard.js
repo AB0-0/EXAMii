3788d8e80db4c252b05b9d5ea6bfb26d7985a411
ABO <140602836+AB0-0@users.noreply.github.com>
2025-07-26 01:53:15 +0800

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -38,9 +38,9 @@ function Dashboard() {
                     <button onClick={()=> preloadExam('progman_m1.csv')}>Progman Module 1</button>
                     <button onClick={()=> preloadExam('progman_m2.csv')}>Progman Module 2</button>
                     <button onClick={()=> preloadExam('progman_m3.csv')}>Progman Module 3</button>
-                    <button onClick={()=> preloadExam('progman_qa.csv')}>Progman Module 3</button>
-                    <button onClick={()=> preloadExam('progman_true_false.csv')}>Progman Module 3</button>
-                    <button onClick={()=> preloadExam('progman_pmroles.csv')}>Progman Module 3</button>
+                    <button onClick={()=> preloadExam('progman_qa.csv')}>Progman Module Q&A</button>
+                    <button onClick={()=> preloadExam('progman_true_false.csv')}>Progman Module True/False</button>
+                    <button onClick={()=> preloadExam('progman_pmroles.csv')}>Progman PM ROLES</button>
                 </div>
             </div>
         </div>
